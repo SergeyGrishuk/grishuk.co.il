6a2bb6979b540dcb602066e7b7efcd5b21a2291c
user-at-host <sergey@grishuk.co.il>
2025-08-27 13:24:37 +0000

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,13 @@
 #!/usr/vin/env python3
 
 
-from .database import Base
-from sqlalchemy import Column, Integer, String, TextField
+from database import Base
+from sqlalchemy import Column, Integer, String, Text
 
 class Project(Base):
     __tablename__ = "projects"
 
     id = Column(Integer, primary_key=True)
-    title = Column(String, index=True)
-    description = Column(TextField)
-    github_link = Column(String, nullable=True)
+    title = Column(String(128), index=True)
+    description = Column(Text)
+    github_link = Column(String(256), nullable=True)
